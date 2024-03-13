c6151c4713fe0403fffaa5b99836c43c4ece5b41
Satya Prakash <satyaprakash99915@gmail.com>
2024-03-13 12:33:57 +0530

    Update NetFuzzer.sh

--- NetFuzzer.sh
+++ NetFuzzer.sh
@@ -122,27 +122,27 @@ if [ -n "$target" ]; then
     case $scan_type in
         live_hosts)
             echo "Discovering live hosts..."
-            smap -sn "$target" -oN live_hosts.txt
+            smap -sn "$target" -oN live_hosts.txt | cat live_hosts.txt
             ;;
         reverse_dns)
             echo "Performing reverse DNS lookup..."
-            smap -R -sL "$target" -oN reverse_dns.txt
+            smap -R -sL "$target" -oN reverse_dns.txt | cat reverse_dns.txt
             ;;
         port_scan)
             echo "Scanning ports and detecting versions..."
-            sudo smap -Pn -sC -sV -T4 -A -O -p0-65535 "$target" -oN port_scan.txt
+            sudo smap -Pn -sC -sV -T4 -A -O -p0-65535 "$target" -oN port_scan.txt | cat port_scan.txt
             ;;
         os_detection)
             echo "Detecting OS..."
-            smap -O "$target" -oN os_detection.txt
+            smap -O "$target" -oN os_detection.txt | cat os_detection.txt
             ;;
         traceroute)
             echo "Performing traceroute..."
-            smap --traceroute "$target" -oN traceroute.txt
+            smap --traceroute "$target" -oN traceroute.txt | cat traceroute.txt
             ;;
         ssl_enum)
             echo "Performing SSL Enumeration..."
-            sudo smap -Pn -sV --script ssl-enum-ciphers -p443 "$target" -oN ssl_enum.txt
+            sudo smap -Pn -sV --script ssl-enum-ciphers -p443 "$target" -oN ssl_enum.txt | cat ssl_enum.txt
             ;;
         smb_enum)
             echo "Performing SMB enumeration using smbclient..."
@@ -163,27 +163,27 @@ if [ -n "$filename" ]; then
     case $scan_type in
         live_hosts)
             echo "Discovering live hosts..."
-            sort "$filename" | uniq | tee "$filename" | xargs -P10 -I{} smap -sn {} -oN live_hosts.txt
+            sort "$filename" | uniq | tee "$filename" | xargs -P10 -I{} smap -sn {} -oN live_hosts.txt | cat live_hosts.txt
             ;;
         reverse_dns)
             echo "Performing reverse DNS lookup..."
-            sort "$filename" | uniq | tee "$filename" | xargs -P10 -I{} sudo smap -R -sL {} -oN reverse_dns.txt
+            sort "$filename" | uniq | tee "$filename" | xargs -P10 -I{} sudo smap -R -sL {} -oN reverse_dns.txt | cat reverse_dns.txt
             ;;
         port_scan)
             echo "Scanning ports and detecting versions..."
-            sort "$filename" | uniq | tee "$filename" | xargs -P10 -I{} sudo smap -Pn -sC -sV -T4 -A -O -p0-65535 {} -oN port_scan.txt
+            sort "$filename" | uniq | tee "$filename" | xargs -P10 -I{} sudo smap -Pn -sC -sV -T4 -A -O -p0-65535 {} -oN port_scan.txt | cat port_scan.txt
             ;;
         os_detection)
             echo "Detecting OS..."
-            sort "$filename" | uniq | tee "$filename" | xargs -P10 -I{} smap -O {} -oN os_detection.txt
+            sort "$filename" | uniq | tee "$filename" | xargs -P10 -I{} smap -O {} -oN os_detection.txt | cat os_detection.txt
             ;;
         traceroute)
             echo "Performing traceroute..."
-            sort "$filename" | uniq | tee "$filename" | xargs -P10 -I{} smap --traceroute {} -oN traceroute.txt
+            sort "$filename" | uniq | tee "$filename" | xargs -P10 -I{} smap --traceroute {} -oN traceroute.txt | cat traceroute.txt
             ;;
         ssl_enum)
             echo "Performing SSL Enumeration..."
-            sort "$filename" | uniq | tee "$filename" | xargs -P10 -I{} sudo smap -Pn -sV --script ssl-enum-ciphers -p443 {} -oN ssl_enum.txt
+            sort "$filename" | uniq | tee "$filename" | xargs -P10 -I{} sudo smap -Pn -sV --script ssl-enum-ciphers -p443 {} -oN ssl_enum.txt | cat ssl_enum.txt
             ;;
         smb_enum)
             echo "Performing SMB enumeration using smbclient..."
